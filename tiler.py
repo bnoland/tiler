e6ffc70d53280498891cec7983872c87ff4e819a
Benjamin Noland <benjaminnoland93@gmail.com>
2020-06-09 23:43:40 -0400

    Some parallax testing.

--- tiler.py
+++ tiler.py
@@ -29,12 +29,21 @@ def main():
     bg_image.fill((0, 255, 0))
     bg_rect = bg_image.get_rect(topleft=(0, round(0.75 * SCREEN_HEIGHT)))
 
+    cloud_image1 = pg.Surface((60, 60))
+    cloud_image1.fill((100, 100, 100))
+    cloud_rect1 = cloud_image1.get_rect(topleft=(100, 200))
+
+    cloud_image2 = pg.Surface((100, 100))
+    cloud_image2.fill((120, 120, 120))
+    cloud_rect2 = cloud_image2.get_rect(topleft=(300, 50))
+
     dt = 1  # Time delta per frame
 
     clock = pg.time.Clock()
 
     running = True
     while running:
+        # TODO: Can't seem to gracefully quit loop based on user input...
         for event in pg.event.get():
             if event.type == pg.KEYDOWN:
                 if event.key == pg.K_ESCAPE:
@@ -53,7 +62,16 @@ def main():
 
         screen.blit(bg_image, bg_rect)
 
+        # TODO: Parallax logic needs to be put into viewport class.
+        old_x = viewport.rect.x
         viewport.update(player)
+        new_x = viewport.rect.x
+        cloud_rect1.x = round(cloud_rect1.x + 0.25 * (new_x - old_x))
+        cloud_rect2.x = round(cloud_rect2.x + 0.5 * (new_x - old_x))
+
+        screen.blit(cloud_image1, cloud_rect1)
+        screen.blit(cloud_image2, cloud_rect2)
+
         viewport.draw(sprites)
 
         pg.display.flip()
